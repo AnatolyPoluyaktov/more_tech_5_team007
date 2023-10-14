964584836bd2afed91fee8fec40ec0d1bc3013fb
Anatoly Poluyaktov <a.poluyaktov@bi.zone>
2023-10-14 22:10:20 +0300

    Fix: fix cors variable

--- backend/src/settings.py
+++ backend/src/settings.py
@@ -33,7 +33,7 @@ INSTALLED_APPS = [
     "src.core.apps.CoreConfig",
     'corsheaders',
 ]
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_ALL_ORIGINS = True
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
